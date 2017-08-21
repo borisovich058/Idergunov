5d22a69a688e73342cad25008c63a0b4fc4eb1b5
Ilya Dergunov <idergunov058@yandex.ru>
2017-08-21 11:30:20 +0300

    Added Hello World

--- src/ru/job4j/Calculate.java
+++ src/ru/job4j/Calculate.java
@@ -1,5 +1,10 @@
 package ru.job4j;
 
+/*
+Author Idergunov(idergunov058@yandex.ru)
+
+*/
+
 public class Calculate {
  public static void main(String[] args) {
     System.out.println("Hello World");
