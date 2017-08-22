4bbd884bf0b03ca339a71fb105995c4495ab7d01
Ilya Dergunov <idergunov058@yandex.ru>
2017-08-22 18:54:03 +0300

    Task #4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,11 +1,17 @@
 package ru.job4j;
 
-/*
-Author Idergunov(idergunov058@yandex.ru)
-
+/**
+*Package for Calculate решение задачи #4087
+*
+*@author Idergunov(idergunov058@yandex.ru)
+*@since 22.08.2017
 */
 
 public class Calculate {
+	/**
+	*Main
+	*@param args - args.
+	*/
  public static void main(String[] args) {
     System.out.println("Hello World");
  }
